2841186084c9951af16a9ccbce5646296b827e0a
sfoutz0205 <68661461+sfoutz0205@users.noreply.github.com>
2020-12-14 13:13:36 -0700

    bundle jk & minify images

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,49 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: './assets/js/script.js',
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events',
+    schedule: './assets/js/schedule',
+    tickets: './assets/js/tickets'
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name (file) {
+                return '[path][name].[ext]'
+              },
+              publicPath: function(url) {
+                return url.replace('../', '/assets/')
+              }
+            }
+          },
+          {
+            loader: 'image-webpack-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins:[
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
     }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', 
+    })
   ],
   mode: 'development'
 };
